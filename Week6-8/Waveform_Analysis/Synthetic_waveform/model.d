537e18b8af6ce3b94aa83232ff0b35c82be5cc02
Ketan Ramaneti <ramanetiketan@gmail.com>
2022-05-02 18:44:24 +0530

    Add files via upload

---
+++ Week6-8/Waveform_Analysis/Synthetic_waveform/model.d
@@ -0,0 +1,7 @@
+simple wholespace model
+ISOTROPIC
+FLAT EARTH
+1-D
+  H(KM) VP(KM/S) VS(KM/S) RHO(GM/CC)   QP   QS  ETAP  ETAS  FREFP  FREFS
+  32.000     6.3000     3.6400     2.700    0.0    0.0   0.0 0.0 1.0 1.0 
+  00.000     8.1000     4.5000     3.500    0.0    0.0   0.0 0.0 1.0 1.0 
